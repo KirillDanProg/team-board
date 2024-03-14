30a38f800fe4c1f00551698fb44b03740add7852
KirillDanProg <kirill2898v@gmail.com>
2024-03-15 00:08:02 +0300

    sidebar added

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const OrganizationSidebar = () => {
+  return (
+    <div className="hidden lg:flex flex-col space-y-6 w-[220px] pl-5 pt-5 bg-slate-400">
+      OrganizationSidebar
+    </div>
+  );
+};
+
+export default OrganizationSidebar;
