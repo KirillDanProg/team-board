5e7c0c51e5a28602d8a9107001d836c68aeabd09
KirillDanProg <kirill2898v@gmail.com>
2024-03-14 22:44:49 +0300

    refactor: convex provider moved in body tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ConvexClientProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ConvexClientProvider>
+    <html lang="ru">
+      <body className={inter.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
+    </html>
   );
 }
