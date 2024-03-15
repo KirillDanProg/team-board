4081f26d99f4eaf3af953c4c9ab40b8364b9f681
KirillDanProg <kirill2898v@gmail.com>
2024-03-15 20:36:04 +0300

    meta data added for dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,13 @@
 import Sidebar from "./_components/sidebar";
 import OrganizationSidebar from "./_components/org-sidebar";
 import Navbar from "./_components/navbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Working dashboard",
+};
+
 interface Props {
   children: React.ReactNode;
 }
@@ -8,10 +15,10 @@ export default function DashboardLayout({ children }: Props) {
   return (
     <main className="h-full">
       <Sidebar />
-      <div className="h-full pl-[80px]">
+      <div className="h-full pl-[60px]">
         <div className="h-full flex gap-x-3 ">
           <OrganizationSidebar />
-          <div className="h-full flex-1 bg-slate-200">
+          <div className="h-full flex-1">
             <Navbar />
             {children}
           </div>
