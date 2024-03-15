768a9641142197decba778a3bf26536cc122ba45
KirillDanProg <kirill2898v@gmail.com>
2024-03-15 20:37:33 +0300

    feat: organization switcher, search input and invite button added to navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,12 +1,43 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton, useOrganization } from "@clerk/nextjs";
 import React from "react";
+import SearchInput from "./search-input";
+import InviteButton from "./invite-button";
 
 const Navbar = () => {
+  const { organization } = useOrganization();
   return (
-    <div className="flex items-center gap-x-4 bg-slate-600 p-5">
-      <div className="hidden lg:flex flex-1 bg-slate-900">search</div>
+    <div className="flex items-center gap-x-4  p-5">
+      <div className="hidden lg:flex flex-1">
+        <SearchInput />
+      </div>
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                alignItems: "center",
+                justify: "center",
+                width: "100%",
+                maxWidth: "300px",
+              },
+              organizationSwitcherTrigger: {
+                justifyContent: "space-between",
+                padding: "6px",
+                width: "100%",
+                border: "1px solid #E5E7EB",
+                borderRadius: "8px",
+              },
+            },
+          }}
+        />
+      </div>
+
+      {organization && <InviteButton />}
+
       <UserButton />
     </div>
   );
