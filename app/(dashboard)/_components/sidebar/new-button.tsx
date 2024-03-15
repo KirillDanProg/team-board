1f973a29b8b95ec1887d3ef740a7ef139ac3ef20
KirillDanProg <kirill2898v@gmail.com>
2024-03-15 09:07:40 +0300

    feat: create new organization button added with hint

---
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { CreateOrganization } from "@clerk/nextjs";
+import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
+import Hint from "@/app/components/hint";
+
+const NewButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="aspect-square">
+          <Hint
+            label="Create organization"
+            side="right"
+            sideOffset={16}
+            align="start"
+            alignOffset={10}
+          >
+            <button className="bg-white/25 h-full w-full rounded-md flex justify-center items-center">
+              <Plus className="text-white" />
+            </button>
+          </Hint>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="max-w-[480px] p-0 bg-transparent border-none">
+        <CreateOrganization />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default NewButton;
