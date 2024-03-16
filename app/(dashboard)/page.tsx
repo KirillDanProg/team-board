59dec914f9f33fd6243c418c44e6d9e736f063aa
KirillDanProg <kirill2898v@gmail.com>
2024-03-16 11:13:26 +0300

    feat: condition added to render board list or empty organization component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,19 @@
-import Image from "next/image";
-import { UserButton } from "@clerk/nextjs";
+"use client";
+import { useOrganization } from "@clerk/nextjs";
+import EmptyOrganization from "./_components/empty-org";
+import BoardList from "./_components/board-list";
 
-export default function Home() {
-  return <div></div>;
+interface Props {
+  searchParams: {
+    query?: string;
+    favorites?: string;
+  };
+}
+export default function Home({ searchParams }: Props) {
+  const { organization } = useOrganization();
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center h-[calc(100%-80px)]">
+      {!organization ? <EmptyOrganization /> : <BoardList query={searchParams} />}
+    </div>
+  );
 }
