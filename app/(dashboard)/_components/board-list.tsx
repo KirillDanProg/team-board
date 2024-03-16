039e240deeb818904c99db6ebe48bd9803895828
KirillDanProg <kirill2898v@gmail.com>
2024-03-16 22:06:06 +0300

    feat: useQuery added to get boards, BoardCard component added

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import Image from "next/image";
 import EmptyResult from "./empty-result";
 import { Button } from "@/components/ui/button";
+import { useQuery } from "convex/react";
+import EmptyBoards from "./empty-boards";
+import { api } from "@/convex/_generated/api";
+import BoardCard from "./board-card";
 
 interface Props {
   query: {
     search?: string;
     favorites?: string;
   };
+  orgId: string;
 }
-const BoardList = ({ query }: Props) => {
-  const data = [];
+const BoardList = ({ query, orgId }: Props) => {
+  const data = useQuery(api.boards.getBoards, { orgId });
+  if (data === undefined) {
+    return <div>Loading...</div>;
+  }
   if (!data.length && query.search) {
     return (
       <EmptyResult
@@ -25,24 +32,36 @@ const BoardList = ({ query }: Props) => {
     return (
       <EmptyResult
         img="/empty-favs.svg"
-        title="В избранных пусто"
-        text="Попробуй добавить доску в избранное"
+        title="Нет избранных бордов"
+        text="Попробуй добавить борд в избранное"
       />
     );
   }
 
   if (!data.length) {
-    return (
-      <EmptyResult
-        img="/hand.svg"
-        title="Создай свою первую доску"
-        text="Начни с создания доски для своего проекта"
-      >
-        <Button size="lg">Создать доску</Button>
-      </EmptyResult>
-    );
+    return <EmptyBoards />;
   }
-  return <div>доски</div>;
+
+  return (
+    <div>
+      <h2 className="text-2xl">{query.favorites ? "Избранные борды" : "Борды"}</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        {data?.map(({ _id, title, authorId, authorName, imageUrl, orgId, _creationTime }) => (
+          <BoardCard
+            key={_id}
+            id={_id}
+            title={title}
+            isFavorite={false}
+            imageUrl={imageUrl}
+            orgId={orgId}
+            authorId={authorId}
+            authorName={authorName}
+            createdAt={_creationTime}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BoardList;
