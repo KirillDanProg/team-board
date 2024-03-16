4cb3b794cf89e3237681f4f59b937aa7cb3c5c0c
KirillDanProg <kirill2898v@gmail.com>
2024-03-16 11:15:12 +0300

    feat: reused empty result component added

---
+++ app/(dashboard)/_components/empty-result.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Image from "next/image";
+
+interface Props {
+  img: string;
+  title: string;
+  text: string;
+  children?: React.ReactNode;
+}
+const EmptyResult = ({ children, title, img, text }: Props) => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Image src={img} width={200} height={200} alt="Empty" />
+      <h2 className="text-2xl semibold mt-6">{title}</h2>
+      <p className="mt-2 text-sm text-muted-foreground">{text}</p>
+      <div className="mt-8">{children}</div>
+    </div>
+  );
+};
+
+export default EmptyResult;
