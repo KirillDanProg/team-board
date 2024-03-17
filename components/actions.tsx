45d6f69f7ba39b5d8b02bd96ab585138d1f04343
KirillDanProg <kirill2898v@gmail.com>
2024-03-17 19:50:57 +0300

    feat: actions button added for board card

---
+++ components/actions.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link2, Trash2 } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+import { toast } from "sonner";
+
+interface Props {
+  id: string;
+  children: React.ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  className?: string;
+}
+const Actions = ({ children, side = "right", sideOffset = 0, id, className }: Props) => {
+  const { mutate, isLoading } = useApiMutation(api.board.deleteBoard);
+
+  const copyBoardLinkHandler = () => {
+    window.navigator.clipboard
+      .writeText(`${window.location.origin}/boards/${id}`)
+      .then(() => toast.success("Ссылка на борд скопирована"))
+      .catch(() => toast.error("Не удалось копировать ссылку"));
+  };
+
+  const deleteBoardHandler = async () => {
+    const res = await mutate({ id });
+    if (!res) {
+      toast.error("Не удалось удалить борд");
+    } else {
+      toast.success("Борд успешно удален!");
+    }
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className={`${className} px-2`} asChild>
+        {children}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(e) => e.stopPropagation()}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+      >
+        <DropdownMenuItem className="cursor-pointer" onClick={copyBoardLinkHandler}>
+          <Link2 className="w-4 h-4 mr-2" />
+          Скопировать ссылку
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer" onClick={deleteBoardHandler}>
+          <Trash2 className="w-4 h-4 mr-2 text-red-600" />
+          Удалить
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default Actions;
