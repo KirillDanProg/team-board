fda65cb612490034560d93fb18d373831736af34
KirillDanProg <kirill2898v@gmail.com>
2024-03-21 11:06:01 +1000

    feat: quering boards by title and favorites added to getBoards query

--- convex/boards.ts
+++ convex/boards.ts
@@ -1,25 +1,56 @@
 import { v } from "convex/values";
 import { query } from "./_generated/server";
+import { getAllOrThrow } from "convex-helpers/server/relationships";
 
 export const getBoards = query({
-  args: { orgId: v.string() },
+  args: {
+    orgId: v.string(),
+    title: v.optional(v.string()),
+    favorites: v.optional(v.string()),
+  },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) {
       throw new Error("Unauthorized");
     }
-
-    const boards = await ctx.db
-      .query("boards")
-      .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
-      .order("desc")
-      .collect();
-
     const userId = identity.subject;
+
+    let boards = [];
+
+    if (args.favorites) {
+      const favoriteBoards = await ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_org", (q) =>
+          q.eq("userId", userId).eq("orgId", args.orgId)
+        )
+        .order("desc")
+        .collect();
+      const favoriteIds = favoriteBoards.map((board) => board.boardId);
+
+      const boards = await getAllOrThrow(ctx.db, favoriteIds);
+      return boards.map((board) => ({ ...board, isFavorite: true }));
+    }
+
+    if (args.title) {
+      boards = await ctx.db
+        .query("boards")
+        .withSearchIndex("search_title", (q) =>
+          q.search("title", args.title!).eq("orgId", args.orgId)
+        )
+        .collect();
+    } else {
+      boards = await ctx.db
+        .query("boards")
+        .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
+        .order("desc")
+        .collect();
+    }
     const boardsWithFavorites = boards.map(async (board) => {
       const favorite = await ctx.db
         .query("userFavorites")
-        .withIndex("by_user_board", (q) => q.eq("boardId", board._id).eq("userId", userId))
+        .withIndex("by_user_board", (q) =>
+          q.eq("userId", userId).eq("boardId", board._id)
+        )
         .unique();
       return { ...board, isFavorite: !!favorite };
     });
