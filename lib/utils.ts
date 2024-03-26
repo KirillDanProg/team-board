11436c52c76329b364a099afef7203df35c1d60d
KirillDanProg <kirill2898v@gmail.com>
2024-03-26 12:38:22 +0300

    feat: util function to get a color by connectionId of user

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const COLORS = [
+  "rgb(249 115 22)",
+  "rgb(250 204 21)",
+  "rgb(132 204 22)",
+  "rgb(45 212 191)",
+  "rgb(56 189 248)",
+  "rgb(79 70 229)",
+  "rgb(232 121 249)",
+  "rgb(225 29 72)",
+];
+export function connectionIdToColor(connectionId: number) {
+  return COLORS[connectionId % COLORS.length];
 }
