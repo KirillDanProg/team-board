580de4d524537ee9cba68e37ab6b3b1aff243ecc
KirillDanProg <kirill2898v@gmail.com>
2024-04-07 23:50:56 +0300

    feat: drafts of others drawing added

--- app/board/[boardId]/_components/cursor/cursor-presence.tsx
+++ app/board/[boardId]/_components/cursor/cursor-presence.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Cursor from "./cursor";
-import { useOthersConnectionIds } from "@/liveblocks.config";
+import { useOthersConnectionIds, useOthersMapped } from "@/liveblocks.config";
+import { shallow } from "@liveblocks/client";
+import Path from "../layers/path";
+import { colorToCss } from "@/lib/utils";
 
 function Cursors() {
   const othersIds = useOthersConnectionIds();
@@ -13,9 +16,38 @@ function Cursors() {
     </>
   );
 }
+function Drafts() {
+  const others = useOthersMapped(
+    (other) => ({
+      pencilDraft: other.presence.pencilDraft,
+      penColor: other.presence.pencilColor,
+    }),
+    shallow
+  );
+  return (
+    <>
+      {/* All the drawing of other users in the room that are currently in progress */}
+      {others.map(([key, other]) => {
+        if (other.pencilDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.pencilDraft}
+              fill={other.penColor ? colorToCss(other.penColor) : "#000"}
+            />
+          );
+        }
+        return null;
+      })}
+    </>
+  );
+}
 function CursorPresence() {
   return (
     <>
+      <Drafts />
       <Cursors />
     </>
   );
