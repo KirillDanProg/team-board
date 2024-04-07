9faa1b9009718926132df3afb0bdc1a4660bee86
KirillDanProg <kirill2898v@gmail.com>
2024-04-08 00:03:56 +0300

    swap added for fonts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -3,9 +3,11 @@ import { Montserrat, Shantell_Sans } from "next/font/google";
 export const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["200", "400", "600", "700"],
+  display: "swap",
 });
 
 export const shantell = Shantell_Sans({
   subsets: ["latin"],
   weight: ["400", "600", "700"],
+  display: "swap",
 });
